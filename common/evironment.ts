f99339c3214924915d9e2c9e9b8f3f4f662299c5
Samuel Pereira de Souza <samuelsouza.contact@gmail.com>
2020-12-18 23:38:47 -0300

    deployment files

--- common/evironment.ts
+++ common/evironment.ts
@@ -1,7 +1,9 @@
 export const environment = {
-    server: {port: process.env.SERVER_PORT || 3001},
-    db: {url: process.env.DB_URL || 'mongodb://localhost:27017/order-api?authSource=root',
-    options:{user:'root', password:'uFmVY4qzIU',useNewUrlParser: true}},
-    security:{saltRounds: process.env.SALT_ROUNDS || 10,
-    apiSecret: process.env.API_SECRET || 'secretKet'}    
+    server: { port: process.env.SERVER_PORT || 3001 },
+    db: {
+        url: process.env.DB_URL || 'mongodb://root:uFmVY4qzIU@cluster0.rcsmz.mongodb.net/cart-api?retryWrites=true&w=majority'},
+    security: {
+        saltRounds: process.env.SALT_ROUNDS || 10,
+        apiSecret: process.env.API_SECRET || 'secretKet'
+    }
 }
