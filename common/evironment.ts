982d4c3d4de0e4fd274c302264c057c6f638b5c0
Samuel Pereira de Souza <samuelsouza.contact@gmail.com>
2020-12-20 00:40:39 -0300

    prd version

--- common/evironment.ts
+++ common/evironment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     server: { port: process.env.SERVER_PORT || 3001 },
     db: {
-        url: process.env.DB_URL || 'mongodb://root:uFmVY4qzIU@cluster0.rcsmz.mongodb.net/cart-api?retryWrites=true&w=majority'},
+        url: process.env.DB_URL || 'mongodb://root:QPExkcCW0W@cluster-1-mongodb/?retryWrites=true&w=majority'},
     security: {
         saltRounds: process.env.SALT_ROUNDS || 10,
         apiSecret: process.env.API_SECRET || 'secretKet'
